32216f979bdad6311d3f570bffea33249f683305
Madlena Ivanova <madlena.iv@abv.bg>
2023-01-03 22:12:00 +0200

    added class sale

---
+++ CarRentingSystemSecond/Data/Models/Sale.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentingSystemSecond.Data.Models
+{
+    public class Sale
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public int Year { get; set; }
+    }
+}
