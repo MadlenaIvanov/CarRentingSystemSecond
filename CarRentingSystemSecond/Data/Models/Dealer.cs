ff764aa034cf50ded409d498ea8c1d70900e64a3
Madlena Ivanova <madlena.iv@abv.bg>
2023-01-13 22:49:54 +0200

    added table dealer

---
+++ CarRentingSystemSecond/Data/Models/Dealer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentingSystemSecond.Data.Models
+{
+    public class Dealer
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Age { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public int Year { get; set; }
+    }
+}
